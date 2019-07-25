3f1aced8ba5c65b07cf9e2f1cb97c5cef8242f11
itzAki07 <valdimor@gmail.com>
2019-07-25 19:15:35 +0530

    Final

---
+++ todo.js
@@ -0,0 +1,27 @@
+//  Strike through when Checked
+
+$('ul').on('click', 'li', function(){
+   $(this).toggleClass('completed');
+})
+
+//  Delete List
+
+$('ul').on('click', 'span', function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+})
+
+$('input[type="text"]').keypress(function(event){
+    if(event.which === 13){
+        var todo = $(this).val();
+        $(this).val("");
+        $('ul').append('<li> <span><i class="fas fa-trash"></i></span> ' + todo + '</li>')
+    }
+
+})
+
+$('.fa-plus').click(function(){
+    $('input[type=text').fadeToggle()
+})
